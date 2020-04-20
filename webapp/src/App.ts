a3c5963f711c7f7fc4d4b8f807d0e953817f215f
Jonathan Bernard <jonathan@jdbernard.com>
2020-04-20 03:48:35 -0500

    Web: Add Font Awesome.

--- webapp/src/App.ts
+++ webapp/src/App.ts
@@ -1,5 +1,15 @@
 import { Component, Vue } from 'vue-property-decorator';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import {
+  faAngleLeft,
+  faAngleRight,
+  faAngleDoubleLeft,
+  faAngleDoubleRight
+} from '@fortawesome/free-solid-svg-icons';
+
+library.add(faAngleLeft, faAngleRight, faAngleDoubleLeft, faAngleDoubleRight);
+
 @Component({})
 export default class App extends Vue {
   public version = process.env.UPCI_WOH_VERSION;
