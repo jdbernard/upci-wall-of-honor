27b0d1ae3dabb7ecfddb0190a7fee094293a1cd6
Jonathan Bernard <jonathan@jdbernard.com>
2020-05-28 22:19:11 -0500

    Use H2's in YearDividers.

--- webapp/src/components/YearDivider.vue
+++ webapp/src/components/YearDivider.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="divider">
-    <img src="../../assets/img/decorative-line-one-side.svg" class="left" />
-    <span class="year-label">{{ year }}</span>
-    <img src="../../assets/img/decorative-line-one-side.svg" class="right" />
+    <img src="../assets/img/decorative-line-one-side.svg" class="left" />
+    <h2 class="year-label">{{ year }}</h2>
+    <img src="../assets/img/decorative-line-one-side.svg" class="right" />
   </div>
 </template>
 <script lang="ts" src="./YearDivider.ts"></script>
