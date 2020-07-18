5921b8157b10843d3d84aebbf1f31570af7dc114
Jonathan Bernard <jonathan@jdbernard.com>
2020-07-18 15:18:58 -0500

    Include more details in the version information embedded in the page.

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -6,6 +6,10 @@ process.env.VUE_APP_UPCI_WOH_VERSION = JSON.stringify(
   require('./package.json').version
 );
 
+process.env.VUE_APP_UPCI_WOH_GIT_HASH = JSON.stringify(
+  require('git-describe').gitDescribeSync('.')
+);
+
 module.exports = {
   devServer: {
     host: 'localhost'
