894b14134afcc88673d7132bfa7681a129e00f3d
Jonathan Bernard <jonathan@jdbernard.com>
2020-08-21 13:57:48 -0500

    admin: Initial stubs for administration UI.

--- webapp/src/router.ts
+++ webapp/src/router.ts
@@ -1,5 +1,8 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
+import AdministrationView from '@/views/admin/Administration.vue';
+import EditMinisterView from '@/views/admin/EditMinister.vue';
+import MinisterTableView from '@/views/admin/MinisterTable.vue';
 import DeceasedMinistersView from '@/views/DeceasedMinisters.vue';
 import OrderOfTheFaithView from '@/views/OrderOfTheFaith.vue';
 import IndexView from '@/views/Index.vue';
@@ -29,6 +32,41 @@ const routes: Array<RouteConfig> = [
     name: 'MinisterBiography',
     meta: { title: "Minister's Biography - UPCI Wall of Honor" },
     component: MinisterBiographyView
+  },
+  {
+    path: '/admin',
+    name: 'Administration',
+    meta: { title: 'Administration - UPCI Wall of Honor' },
+    component: AdministrationView,
+    children: [
+      { path: '', redirect: { name: 'AdminAllMinistersTable' } },
+      {
+        path: 'all-ministers',
+        name: 'AdminAllMinistersTable',
+        component: MinisterTableView,
+        meta: { title: 'All Ministers - UPCI Wall of Honor' },
+        props: { filter: 'all' }
+      },
+      {
+        path: 'order-of-the-faith',
+        name: 'AdminOrderOfTheFaithTable',
+        component: MinisterTableView,
+        meta: { title: 'Order of the Faith - UPCI Wall of Honor' },
+        props: { filter: 'ootf' }
+      },
+      {
+        path: 'edit-minister/:slug',
+        name: 'AdminEditMinister',
+        component: EditMinisterView,
+        meta: { title: 'Edit Minister - UPCI Wall of Honor' }
+      },
+      {
+        path: 'add-minister',
+        name: 'AdminAddUser',
+        component: EditMinisterView,
+        meta: { title: 'Add Minister - UPCI Wall of Honor' }
+      }
+    ]
   },
   {
     path: '/',
---
+++ webapp/src/views/admin/EditMinister.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>EditMinister</div>
+</template>
+<script lang="ts" src="./EditMinister.ts"></script>
+<style lang="scss" src="./EditMinister.scss"></style>
