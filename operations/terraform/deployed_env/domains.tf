1b7eadc8c580501cc6d9a6c7163e2e5f27491e17
Jonathan Bernard <jonathan@jdbernard.com>
2020-08-31 04:17:59 -0500

    operations: WIP refactoring to add Cognito, API Gateway, DynamoDB, etc.

---
+++ operations/terraform/deployed_env/domains.tf
@@ -0,0 +1,80 @@
+resource "aws_acm_certificate" "cert" {
+  provider          = aws.cert
+  domain_name       = "${var.environment}.${trimsuffix(var.route53_zone.name, ".")}"
+  validation_method = "DNS"
+
+  subject_alternative_names = concat(var.additional_cloudfront_aliases, [local.api_domain_name])
+
+  tags = {
+    Environment = var.environment
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name    = dvo.resource_record_name
+      type    = dvo.resource_record_type
+      record = dvo.resource_record_value
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [ each.value.record ]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.route53_zone.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cert" {
+  provider                = aws.cert
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
+
+resource "aws_route53_record" "subdomain" {
+  count = var.create_env_subdomain ? 1: 0
+  zone_id = var.route53_zone.zone_id
+  name = "${var.environment}.${trimsuffix(var.route53_zone.name, ".")}"
+  type = "A"
+
+  alias {
+    name    = aws_cloudfront_distribution.cdn.domain_name
+    zone_id = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+
+  depends_on = [aws_cloudfront_distribution.cdn]
+}
+
+resource "aws_route53_record" "api_domain" {
+  name = local.api_domain_name
+  type = "A"
+  zone_id = var.route53_zone.zone_id
+
+  alias {
+    evaluate_target_health  = true
+    name                    = aws_api_gateway_domain_name.api.cloudfront_domain_name
+    zone_id                 = aws_api_gateway_domain_name.api.cloudfront_zone_id
+  }
+}
+
+resource "aws_route53_record" "alternate_domains" {
+  count   = length(var.additional_cloudfront_aliases)
+  zone_id = var.route53_zone.zone_id
+  name    = var.additional_cloudfront_aliases[count.index]
+  type    = "A"
+
+  alias {
+    name    = aws_cloudfront_distribution.cdn.domain_name
+    zone_id = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+
+  depends_on = [aws_cloudfront_distribution.cdn]
+}
---
+++ operations/terraform/deployed_env/hosting.tf
@@ -0,0 +1,123 @@
+resource "aws_s3_bucket" "deploy" {
+  bucket        = "${var.environment}.${trimsuffix(var.route53_zone.name, ".")}"
+  acl           = "private"
+  force_destroy = true
+
+  logging {
+    target_bucket = var.project_bucket_id
+    target_prefix = "logs/${var.environment}/s3-access"
+  }
+
+  tags = {
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "deploy" {
+  bucket = aws_s3_bucket.deploy.id
+
+  block_public_acls   = true
+  block_public_policy = true
+}
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "OAI for ${var.environment} environment."
+}
+
+data "aws_iam_policy_document" "deploy_bucket_access_policy" {
+  statement {
+    actions   = [ "s3:GetObject" ]
+    effect    = "Allow"
+    resources = [ "${aws_s3_bucket.deploy.arn}/*" ]
+
+    principals {
+      type = "AWS"
+      identifiers = [ aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn ]
+    }
+  }
+
+  statement {
+    actions   = [ "s3:ListBucket" ]
+    effect    = "Allow"
+    resources = [ aws_s3_bucket.deploy.arn ]
+
+    principals {
+      type = "AWS"
+      identifiers = [ aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn ]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "deploy_bucket_access_policy" {
+  bucket = aws_s3_bucket.deploy.id
+  policy = data.aws_iam_policy_document.deploy_bucket_access_policy.json
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.deploy.bucket_regional_domain_name
+    origin_id = "S3-${aws_s3_bucket.deploy.id}"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "UPCI Wall of Honor ${var.environment} distribution."
+  default_root_object = "index.html"
+
+  logging_config {
+    include_cookies = false
+    bucket          = "${var.project_bucket_id}.s3.amazonaws.com"
+    prefix          = "logs/${var.environment}/cloudfront/"
+  }
+
+  aliases = concat(["${var.environment}.${trimsuffix(var.route53_zone.name, ".")}"], var.additional_cloudfront_aliases)
+
+  default_cache_behavior {
+    allowed_methods   = [ "GET", "HEAD", "OPTIONS" ]
+    cached_methods    = [ "GET", "HEAD" ]
+    target_origin_id  = "S3-${aws_s3_bucket.deploy.id}"
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                 = 0
+    default_ttl             = 31536000 # default to a year
+    max_ttl                 = 31536000
+    compress                = true
+    viewer_protocol_policy  = "redirect-to-https"
+  }
+
+  custom_error_response {
+    error_code          = 404
+    response_code       = 200
+    response_page_path  = "/index.html"
+  }
+
+  price_class = "PriceClass_All"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Environment = var.environment
+  }
+
+  viewer_certificate {
+    acm_certificate_arn   = aws_acm_certificate.cert.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  depends_on = [ aws_acm_certificate_validation.cert ]
+}
