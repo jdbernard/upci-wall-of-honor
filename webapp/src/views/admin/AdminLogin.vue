3c29a50eab4191eb772577ef2e461916cbcb5da3
Jonathan Bernard <jonathan@jdbernard.com>
2020-09-06 22:26:35 -0500

    webapp: WIP initial stab at a login screen.

---
+++ webapp/src/views/admin/AdminLogin.vue
@@ -0,0 +1,26 @@
+<template>
+  <form id="admin-login" class="theme-admin">
+    <UPCILogo></UPCILogo>
+    <h1>Wall of Honor</h1>
+    <h2>Administration</h2>
+    <input
+      type="text"
+      placeholder="username"
+      name="username"
+      :model="username"
+      required
+    />
+    <input
+      type="password"
+      placeholder="password"
+      name="password"
+      :model="password"
+      required
+    />
+    <button class="action" @click.prevent="login">
+      Login
+    </button>
+  </form>
+</template>
+<script lang="ts" src="./AdminLogin.ts"></script>
+<style lang="scss" src="./AdminLogin.scss"></style>
