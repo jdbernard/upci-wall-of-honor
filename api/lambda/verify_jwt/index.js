42ad22e7aaee35dfb8d76afc8bcf7af780ca6e73
Jonathan Bernard <jonathan@jdbernard.com>
2020-09-10 07:12:30 -0500

    api: JWT verifier should parse Authorization header.

--- api/lambda/verify_jwt/index.js
+++ api/lambda/verify_jwt/index.js
@@ -17,16 +17,23 @@ function generatePolicy(principalId, methodArn) {
 }
 
 exports.handler = async function(event, context, callback) {
-  const token = event.authorizationToken;
+  console.log('api-endpoint=' + event.methodArn);
+  const headerVal = event.authorizationToken;
 
-  try {
-    const apiId = event.methodArn.match(/^([^:]+:){5}([^:/]+)\//)[2];
+  if (!headerVal.startsWith('Bearer ')) {
+    throw('Error: Invalid Token (not a bearer token)');
+  }
 
-    const oktaJwtVerifier = new OktaJwtVerifier({
-      issuer: environments[apiId].issuer
-    });
+  const token = headerVal.substr(7);
+  const apiId = event.methodArn.match(/^([^:]+:){5}([^:/]+)\//)[2];
+
+  const oktaJwtVerifier = new OktaJwtVerifier({
+    issuer: environments[apiId].issuer
+  });
   
+  try {
     const jwt = await oktaJwtVerifier.verifyAccessToken(token, 'api://default');
+    console.log('Valid token. email=' + jwt.claims.sub);
     return generatePolicy(jwt.claims.sub, event.methodArn);
   } catch (err) {
     console.log("Auth error: " + JSON.stringify(err));
