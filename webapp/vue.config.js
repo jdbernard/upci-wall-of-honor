132a4e856dbf4779d0ae60f4a7514197bb43d2dd
Jonathan Bernard <jonathan@jdbernard.com>
2020-12-08 22:41:58 -0600

    webapp: Disabling pre-rendering until I can figure out why it's broken.

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -7,30 +7,5 @@ process.env.VUE_APP_UPCI_WOH_GIT_HASH = JSON.stringify(
 module.exports = {
   devServer: {
     host: 'localhost'
-  },
-
-  pluginOptions: {
-    prerenderSpa: {
-      registry: undefined,
-      renderRoutes: [
-        '/',
-        '/deceased-ministers/',
-        '/order-of-the-faith/',
-        '/leadership/',
-        '/leadership/executive/',
-        '/leadership/ministries/',
-        '/leadership/general-board/',
-        '/admin/',
-        '/admin/all-ministers/',
-        '/admin/order-of-the-faith/',
-        '/admin/leadership/',
-        '/admin/leadership/executive/',
-        '/admin/leadership/ministries/',
-        '/admin/leadership/general-board/'
-      ],
-      useRenderEvent: false,
-      headless: true,
-      onlyProduction: true
-    }
   }
 };
